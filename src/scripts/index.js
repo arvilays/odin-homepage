b0ba334238ef62e9b0f0159bb2bea1cf56a5be0a
arvilays <123064316+arvilays@users.noreply.github.com>
2025-03-14 18:52:33 -0700

    Initial commit

---
+++ src/scripts/index.js
@@ -0,0 +1,12 @@
+import "../css/reset.css";
+import "../css/style.css";
+import Events from "./events.js";
+
+const main = () => {};
+
+main();
+
+/* 
+ * Use the command below to reinstall packages:
+ * npm run reinstall
+ */
